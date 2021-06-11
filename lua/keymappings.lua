56fe3708dfab061836c27b495089aab97f0cf790
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 06:59:50 +0800

    refactor: Capitalize comment in keymappings

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -1,11 +1,11 @@
 local u = require("utils")
 
--- explorer
+-- Explorer
 u.map('n', '<C-n>', ':NvimTreeToggle<CR>')
 u.map('n', 'go', ':NvimTreeFindFile<CR>')
 
--- buffer
+-- Buffer
 u.map('n', 'X', ':Bdelete<CR>')
 
--- maximizer
+-- Maximizer
 u.map('n', '<C-w>m', ':MaximizerToggle!<cr>')

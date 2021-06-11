c8973068982abd84c4898c6c0ff6925f826495ad
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 06:25:03 +0800

    feat: Add gitsigns config

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,1 +1,22 @@
-require("gitsigns").setup()
+require('gitsigns').setup {
+    signs = {
+        delete = {hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn'},
+        topdelete = {hl = 'GitSignsDelete', text = '契', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn'},
+    },
+    numhl = false,
+    linehl = false,
+    keymaps = {
+        -- Default keymap options
+        noremap = true,
+        buffer = true,
+        ['n ]c'] = {expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns\".next_hunk()<CR>zz'"},
+        ['n [c'] = {expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<CR>zz'"}, -- Text objects
+        ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+        ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+    },
+    watch_index = {interval = 1000},
+    sign_priority = 6,
+    update_debounce = 200,
+    status_formatter = nil, -- Use default
+    use_decoration_api = false
+}

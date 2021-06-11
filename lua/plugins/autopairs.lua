1b5775c7978ed13baa35e58539cb2c2cc8404cd6
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 06:22:26 +0800

    feat: Add autopairs custom rule

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,4 +1,5 @@
 local npairs = require("nvim-autopairs")
+local Rule = require('nvim-autopairs.rule')
 local u = require("utils")
 
 npairs.setup({
@@ -11,3 +12,16 @@ _G.global.on_enter = function()
     return npairs.autopairs_cr()
 end
 u.map("i", "<CR>", "v:lua.global.on_enter()", { expr = true })
+
+-- Add spaces between parentheses
+npairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col, opts.col + 1)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ',' )')
+        :with_pair(function() return false end)
+        :with_move(function() return true end)
+        :use_key(")")
+}

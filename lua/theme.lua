c0955b6de7907132a161972fd25f81413a91a6fd
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 09:31:12 +0800

    feat: Update tokyonight theme setting

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,3 +1,6 @@
 vim.cmd("colorscheme tokyonight")
 vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_terminal_colors = true
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_colors = {hint = "orange", error = "#ff0000"}

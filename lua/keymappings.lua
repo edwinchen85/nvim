41af1616e96438c9c69c9438fd861864e44004bc
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 16:14:28 +0800

    refactor: Use utility set key map function

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -59,22 +59,22 @@ u.map('n', '<Down>', 'gj')
 u.map('n', '<Up>', 'gk')
 
 -- Sent to black hole register
-vim.api.nvim_set_keymap('n', 'cc', '"_cc', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'cl', '"_cl', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'c', '"_c', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', 'c', '"_c', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'C', '"_C', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 's', '"_s', {noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', 'S', '"_S', {noremap = true, silent = true})
+u.map('n', 'cc', '"_cc')
+u.map('n', 'cl', '"_cl')
+u.map('n', 'c', '"_c')
+u.map('v', 'c', '"_c')
+u.map('n', 'C', '"_C')
+u.map('n', 's', '"_s')
+u.map('n', 'S', '"_S')
 
 -- Use . in visual mode to execute the dot command on each selected line
-vim.api.nvim_set_keymap('x', '.', ':normal .<CR>', {noremap = true})
+u.map('x', '.', ':normal .<CR>')
 
 -- Delete last argument in comma separated list
-vim.api.nvim_set_keymap('n', 'dge', 'EdgE', {noremap = true, silent = true})
+u.map('n', 'dge', 'EdgE')
 
 -- Avoid unintentional switches to Ex mode
-vim.api.nvim_set_keymap('n', 'Q', '<NOP>', {noremap = true, silent = true})
+u.map('n', 'Q', '<NOP>')
 
 -- Can use <space> or <cr> to terminate wildmenu
 vim.cmd('cnoremap <expr> <space> wildmenumode() ? \"\\<C-y>\" : \"\\<space>\"')

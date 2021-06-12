5306d07033dd4b63e7b302b1f23cbe6887d6fb7e
Edwin Chen <edwin.chen@aquariux.com>
2021-06-12 09:28:33 +0800

    feat: Extend telescope default config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,7 +12,52 @@ telescope.setup({
     extensions = {
         fzf = { override_generic_sorter = true, override_file_sorter = true },
     },
-    defaults = { mappings = { i = { ["<Esc>"] = actions.close, ["<C-u>"] = false } } },
+    defaults = {
+        find_command = {'rg', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case'},
+        prompt_position = "bottom",
+        prompt_prefix = " ",
+        selection_caret = " ",
+        entry_prefix = "  ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "descending",
+        layout_strategy = "horizontal",
+        layout_defaults = {horizontal = {mirror = false}, vertical = {mirror = false}},
+        file_sorter = require'telescope.sorters'.get_fzy_sorter,
+        file_ignore_patterns = {},
+        generic_sorter = require'telescope.sorters'.get_generic_fuzzy_sorter,
+        shorten_path = true,
+        winblend = 0,
+        width = 0.75,
+        preview_cutoff = 120,
+        results_height = 1,
+        results_width = 0.8,
+        border = {},
+        borderchars = {'─', '│', '─', '│', '╭', '╮', '╯', '╰'},
+        color_devicons = true,
+        use_less = true,
+        set_env = {['COLORTERM'] = 'truecolor'}, -- default = nil,
+        file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
+        grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
+        qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
+
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker,
+        mappings = {
+            i = {
+                ["<C-c>"] = actions.close,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                ["<CR>"] = actions.select_default + actions.center
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist
+            }
+        }
+    },
 })
 
 _G.global.telescope = {

6481c6b13f56154456c047092d8903b0991cf4ca
Edwin Chen <edwin.chen@aquariux.com>
2021-06-25 10:58:50 +0800

    refactor: Speed up plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,10 +3,10 @@ return require("packer").startup(function()
     use({ "wbthomason/packer.nvim", opt = true })
 
     local config = function(name)
-        pcall(require, "plugins." .. name)
+        require("plugins." .. name)
     end
-    local use_with_config = function(opts)
-        local path, name = opts[1], opts[2]
+
+    local use_with_config = function(path, name)
         use({ path, config = config(name) })
     end
 
@@ -22,21 +22,21 @@ return require("packer").startup(function()
         "tpope/vim-fugitive",
         { "tpope/vim-rhubarb", "junegunn/gv.vim", "tommcdo/vim-fugitive-blame-ext" },
     })
-    use_with_config({ "lewis6991/gitsigns.nvim", "gitsigns" })
+    use_with_config("lewis6991/gitsigns.nvim", "gitsigns")
 
     -- custom
-    use_with_config({ "kyazdani42/nvim-tree.lua", "nvimtree" })
-    use_with_config({ "cappyzawa/trim.nvim", "trim" })
-    use_with_config({ "bkad/CamelCaseMotion", "camelcasemotion" })
-    use_with_config({ "folke/which-key.nvim", "whichkey" })
-    use_with_config({ "folke/todo-comments.nvim", "todocomments" })
+    use_with_config("kyazdani42/nvim-tree.lua", "nvimtree")
+    use_with_config("cappyzawa/trim.nvim", "trim")
+    use_with_config("bkad/CamelCaseMotion", "camelcasemotion")
+    use_with_config("folke/which-key.nvim", "whichkey")
+    use_with_config("folke/todo-comments.nvim", "todocomments")
     use({ "jeffkreeftmeijer/vim-numbertoggle" })
     use({ "lambdalisue/suda.vim" })
     use({ "andymass/vim-matchup" })
     use({ "szw/vim-maximizer" })
 
     -- text objects
-    use("wellle/targets.vim")
+    use("wellle/targets.vim") -- many useful additional text objects
     use({
         "kana/vim-textobj-user",
         {
@@ -49,17 +49,17 @@ return require("packer").startup(function()
     })
 
     -- additional functionality
-    use_with_config({ "windwp/nvim-autopairs", "autopairs" }) -- autocomplete pairs
+    use_with_config("windwp/nvim-autopairs", "autopairs") -- autocomplete pairs
     use({
         "nvim-telescope/telescope.nvim",
         requires = { "nvim-lua/popup.nvim" },
         config = config("telescope"),
     })
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- better algorithm
+    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- better search algorithm
 
     -- integrations
-    use_with_config({ "mcchrish/nnn.vim", "nnn" }) -- file manager integration
-    use_with_config({ "christoomey/vim-tmux-runner", "vtr" }) -- run commands in a linked tmux pane
+    use_with_config("mcchrish/nnn.vim", "nnn") -- file manager integration
+    use_with_config("christoomey/vim-tmux-runner", "vtr") -- run commands in a linked tmux pane
 
     -- development
     use("neovim/nvim-lspconfig")
@@ -73,23 +73,23 @@ return require("packer").startup(function()
     use("RRethy/nvim-treesitter-textsubjects") -- adds smart . text object
     use("JoosepAlviste/nvim-ts-context-commentstring") -- makes jsx comments actually work
     use("windwp/nvim-ts-autotag") -- autocomplete jsx tags
-    use_with_config({ "ahmedkhalf/lsp-rooter.nvim", "rooter" }) -- automagically change working directory
+    use_with_config("ahmedkhalf/lsp-rooter.nvim", "rooter") -- automagically change working directory
 
     -- autocomplete
-    use_with_config({ "hrsh7th/nvim-compe", "compe" })
+    use_with_config("hrsh7th/nvim-compe", "compe")
     use({ "hrsh7th/vim-vsnip" })
     use({ "rafamadriz/friendly-snippets" })
 
     -- visual
     use("folke/tokyonight.nvim")
     use("kyazdani42/nvim-web-devicons")
-    use_with_config({ "RRethy/vim-illuminate", "illuminate" }) -- highlight and jump between references
-    use_with_config({"glepnir/dashboard-nvim", "dashboard"})
-    use_with_config({"glepnir/galaxyline.nvim", "galaxyline"})
-    use_with_config({"norcalli/nvim-colorizer.lua", "colorizer"})
+    use_with_config("RRethy/vim-illuminate", "illuminate") -- highlight and jump between references
+    use_with_config("glepnir/dashboard-nvim", "dashboard")
+    use_with_config("glepnir/galaxyline.nvim", "galaxyline")
+    use_with_config("norcalli/nvim-colorizer.lua", "colorizer")
 
     -- local
-    use_with_config({ "jose-elias-alvarez/buftabline.nvim", "buftabline" })
+    use_with_config("jose-elias-alvarez/buftabline.nvim", "buftabline")
     use("jose-elias-alvarez/nvim-lsp-ts-utils")
     use("jose-elias-alvarez/null-ls.nvim")
 

be75828ba4312ad04123a46e820f811104599d7a
Edwin Chen <edwin.chen@aquariux.com>
2021-06-25 11:56:58 +0800

    feat: Update null-ls config sources

--- lua/lsp/null-ls.lua
+++ lua/lsp/null-ls.lua
@@ -7,8 +7,10 @@ local sources = {
     }),
     b.formatting.stylua.with({ args = { "--config-path", vim.fn.stdpath("config") .. "/lua/stylua.toml", "-" } }),
     b.formatting.trim_whitespace.with({ filetypes = { "tmux" } }),
+    b.formatting.shfmt,
     b.diagnostics.write_good,
     b.diagnostics.markdownlint,
+    b.diagnostics.shellcheck,
     b.code_actions.gitsigns,
 }
 

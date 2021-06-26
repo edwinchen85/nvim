d7f1c85595d8445dc2059eb3273df96fbb777648
Edwin Chen <edwin.chen@aquariux.com>
2021-06-27 07:13:21 +0800

    feat: Show icon in buftabline

--- lua/plugins/buftabline.lua
+++ lua/plugins/buftabline.lua
@@ -2,4 +2,7 @@ require("buftabline").setup({
     kill_maps = true,
     custom_command = "vertical sb",
     custom_map_prefix = "v",
+    icons = true,
+    icon_colors = "normal",
+    auto_hide = true,
 })

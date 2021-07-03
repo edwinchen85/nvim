cb9b2c82e60ab47bd593701136df7c49d2a14f91
Edwin Chen <edwin.chen@aquariux.com>
2021-07-03 11:29:16 +0800

    feat: Edit nvim-tree config after version update

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -9,37 +9,13 @@ vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent ma
 vim.g.nvim_tree_follow = 0 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
 vim.g.nvim_tree_auto_ignore_ft = {"startify", "dashboard"} -- empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_root_folder_modifier = ":t" -- :~ by default, root folder display format
+
 local tree_cb = require"nvim-tree.config".nvim_tree_callback
 vim.g.nvim_tree_bindings = {
-    -- default mappings
-    ["<CR>"] = tree_cb("edit"),
-    ["o"] = tree_cb("edit"),
-    ["l"] = tree_cb("edit"),
-    ["<2-LeftMouse>"] = tree_cb("edit"),
-    ["<2-RightMouse>"] = tree_cb("cd"),
-    ["<C-v>"] = tree_cb("vsplit"),
-    ["<C-x>"] = tree_cb("split"),
-    ["<C-t>"] = tree_cb("tabnew"),
-    ["<"] = tree_cb("prev_sibling"),
-    [">"] = tree_cb("next_sibling"),
-    ["<BS>"] = tree_cb("close_node"),
-    ["h"] = tree_cb("close_node"),
-    ["<S-CR>"] = tree_cb("close_node"),
-    ["<Tab>"] = tree_cb("preview"),
-    ["I"] = tree_cb("toggle_ignored"),
-    ["H"] = tree_cb("toggle_dotfiles"),
-    ["R"] = tree_cb("refresh"),
-    ["a"] = tree_cb("create"),
-    ["d"] = tree_cb("remove"),
-    ["r"] = tree_cb("rename"),
-    ["<C-r>"] = tree_cb("full_rename"),
-    ["x"] = tree_cb("cut"),
-    ["c"] = tree_cb("copy"),
-    ["p"] = tree_cb("paste"),
-    ["[c"] = tree_cb("prev_git_item"),
-    ["]c"] = tree_cb("next_git_item"),
-    ["-"] = tree_cb("dir_up"),
-    ["q"] = tree_cb("close")
+    {key = {"l", "<CR>", "o"}, cb = tree_cb("edit")},
+    {key = "h", cb = tree_cb("close_node")},
+    {key = "v", cb = tree_cb("vsplit")}
 }
 
 vim.g.nvim_tree_icons = {
@@ -47,10 +23,12 @@ vim.g.nvim_tree_icons = {
     symlink = "",
     git = {
         unstaged = "",
-        staged = "✓",
+        staged = "S",
         unmerged = "",
         renamed = "➜",
-        untracked = ""
+        untracked = "U",
+        deleted = "",
+        ignored = "◌"
     },
     folder = {
         default = "",

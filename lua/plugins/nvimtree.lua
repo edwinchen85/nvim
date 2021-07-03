e7efead536c60673a80f5acb67e015cdebcbc2f4
Edwin Chen <edwin.chen@aquariux.com>
2021-07-03 09:49:45 +0800

    feat: Add dashboard in nvim-tree ignore filetype

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -8,7 +8,7 @@ vim.g.nvim_tree_hide_dotfiles = 1 -- 0 by default, this option hides files and f
 vim.g.nvim_tree_indent_markers = 1 -- "0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_follow = 0 -- "0 by default, this option allows the cursor to be updated when entering a buffer
 vim.g.nvim_tree_auto_close = 1 -- 0 by default, closes the tree when it's the last window
-vim.g.nvim_tree_auto_ignore_ft = "startify" -- empty by default, don't auto open tree on specific filetypes.
+vim.g.nvim_tree_auto_ignore_ft = {"startify", "dashboard"} -- empty by default, don't auto open tree on specific filetypes.
 local tree_cb = require"nvim-tree.config".nvim_tree_callback
 vim.g.nvim_tree_bindings = {
     -- default mappings

c5fcdee7dc1d880c813baa3cd74072cdd85571a0
Edwin Chen <edwin.chen@aquariux.com>
2021-08-03 04:02:59 +0800

    refactor: Update autopairs custom rule

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -44,12 +44,26 @@ npairs.add_rules({
 -- Add spaces between parentheses
 npairs.add_rules {
   Rule(' ', ' ')
-    :with_pair(function (opts)
-      local pair = opts.line:sub(opts.col, opts.col + 1)
-      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-    end),
-  Rule('( ',' )')
-        :with_pair(function() return false end)
-        :with_move(function() return true end)
-        :use_key(")")
+  :with_pair(function (opts)
+    local pair = opts.line:sub(opts.col - 1, opts.col)
+    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+  end),
+Rule('( ', ' )')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+        return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+Rule('{ ', ' }')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+        return opts.prev_char:match('.%}') ~= nil
+    end)
+    :use_key('}'),
+Rule('[ ', ' ]')
+    :with_pair(function() return false end)
+    :with_move(function(opts)
+        return opts.prev_char:match('.%]') ~= nil
+    end)
+    :use_key(']')
 }

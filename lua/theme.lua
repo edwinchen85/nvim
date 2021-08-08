fe95835824f209cb55c033e9aef2631e2212e5d0
Edwin Chen <edwin.chen@aquariux.com>
2021-08-08 16:06:04 +0800

    feat: Use storm as tokyonight style

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,5 +1,5 @@
 vim.cmd("colorscheme tokyonight")
-vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_style = "storm" -- 'storm, night, day'
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_terminal_colors = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }

32b2c5ccc08fb2e94ee9c304815a751d7752e3d7
Edwin Chen <edwin.chen@aquariux.com>
2021-08-10 07:32:19 +0800

    refactor: Trim buftabline config

--- lua/plugins/buftabline.lua
+++ lua/plugins/buftabline.lua
@@ -1,8 +1,4 @@
 require("buftabline").setup({
-    kill_maps = true,
-    custom_command = "vertical sb",
-    custom_map_prefix = "v",
-    icons = true,
-    icon_colors = "normal",
+    tabpage_buffers = true,
     auto_hide = true,
 })

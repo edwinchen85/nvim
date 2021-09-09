6d595020c2823c2d962da4d3f5ac451ab033cefe
Edwin Chen <edwin.chen@aquariux.com>
2021-09-09 13:32:28 +0800

    feat: Comment and add vim options

--- init.lua
+++ init.lua
@@ -1,39 +1,54 @@
 vim.g.mapleader = " "
 
+vim.opt.backspace = { "indent", "eol", "start" }
 vim.opt.clipboard = "unnamedplus"
-vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
-vim.opt.expandtab = true
+vim.opt.encoding = 'utf-8' -- Set default encoding to UTF-8
+vim.opt.completeopt = { "menuone", "noselect" }
+vim.opt.expandtab = true -- Uses spaces instead of tabs
+vim.opt.foldenable = false
+vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 99
-vim.opt.hidden = true
-vim.opt.ignorecase = true
+vim.opt.hidden = true -- Enable background buffers
+vim.opt.hlsearch = true -- Highlight found searches
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.incsearch = true -- Shows the match while typing
+vim.opt.inccommand = "split" -- Shows the match while typing
+vim.opt.joinspaces = false -- No double spaces with join
+vim.opt.linebreak = true -- Stop words being broken on wrap
+vim.opt.list = false -- Show some invisible characters
+vim.opt.cc = "120"
 vim.opt.mouse = "a"
 vim.opt.pumheight = 10
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 4 -- Size of indent
+vim.opt.shiftround = true -- Round indent
 vim.opt.showcmd = false
+vim.opt.showmode = false -- Don't display mode
 vim.opt.showtabline = 2
-vim.opt.smartcase = true
-vim.opt.smartindent = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
+vim.opt.smartcase = true -- Do not ignore case with capitals
+vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.spelllang = "en"
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
 vim.opt.diffopt = "vertical,filler"
 vim.opt.statusline = [[%f %y %m %= %p%% %l:%c]]
 vim.opt.tabstop = 4
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true -- You will have bad experience for diagnostic messages when it's default 4000.
+vim.opt.wrap = true
 vim.opt.undofile = true
 vim.opt.updatetime = 250
 vim.opt.timeoutlen = 500
 vim.opt.writebackup = false
 vim.opt.swapfile = false
 vim.opt.directory = "/tmp"
-vim.opt.scrolloff = 4
-vim.opt.sidescrolloff = 2
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.sidescrolloff = 8 -- Columns of context
 vim.opt.shell = "/bin/sh"
 vim.opt.shortmess:append("cA")
 
 vim.opt.cursorline = true
-vim.opt.number = true
+vim.opt.number = true -- Show line numbers
 vim.opt.relativenumber = true
-vim.opt.signcolumn = "yes"
+vim.opt.signcolumn = "yes:1" -- Always show signcolumn
 vim.opt.backupcopy = "yes"
 
 -- the only way I've found to make this persistent

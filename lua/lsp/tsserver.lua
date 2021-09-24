f67f1ecb8c16d5029de6e1c7ab8c6594850df46f
Edwin Chen <edwin.chen@aquariux.com>
2021-09-24 08:43:45 +0800

    fix: Add missing bufnr arg in on_attach function

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -30,7 +30,8 @@ M.setup = function(on_attach)
         on_attach = function(client, bufnr)
             client.resolved_capabilities.document_formatting = false
             client.resolved_capabilities.document_range_formatting = false
-            on_attach(client)
+
+            on_attach(client, bufnr)
 
             local ts_utils = require("nvim-lsp-ts-utils")
             ts_utils.setup(ts_utils_settings)

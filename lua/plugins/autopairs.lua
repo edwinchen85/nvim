1348ce451da156c4ba6d943b01b3f4e5165597df
Edwin Chen <edwin.chen@aquariux.com>
2021-11-01 19:04:08 +0800

    refactor: Update autopairs config after version up

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,31 +1,10 @@
 local npairs = require("nvim-autopairs")
 local Rule = require('nvim-autopairs.rule')
-local u = require("utils")
 
-npairs.setup({
+require("nvim-autopairs").setup({
     check_ts = true,
-    ignored_next_char = "[%w%.]",
-    check_line_pair = false,
-    ts_config = {
-        lua = {'string'},-- it will not add pair on that treesitter node
-        javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
+    map_c_w = true,
 })
-
-_G.global.on_enter = function()
-    if vim.fn.pumvisible() ~= 0  then
-      if vim.fn.complete_info()["selected"] ~= -1 then
-        return vim.fn["compe#confirm"](npairs.esc("<cr>"))
-      else
-        return npairs.esc("<cr>")
-      end
-    else
-      return npairs.autopairs_cr()
-    end
-end
-
-u.map("i", "<CR>", "v:lua.global.on_enter()", { expr = true })
 
 require('nvim-treesitter.configs').setup {
     autopairs = {enable = true}

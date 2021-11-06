fef49e9b2f4751eae6a3939f96019cca9b977fb3
Edwin Chen <edwin.chen@aquariux.com>
2021-11-07 07:50:42 +0800

    feat: Customise cmp menu format

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -73,6 +73,13 @@ cmp.setup({
     formatting = {
         format = function(entry, vim_item)
             vim_item.kind = string.format("%s %s", lspkind.presets.default[vim_item.kind], vim_item.kind)
+            vim_item.menu = ({
+                nvim_lsp = "(LSP)",
+                emoji = "(Emoji)",
+                path = "(Path)",
+                vsnip = "(Snippet)",
+                buffer = "(Buffer)",
+            })[entry.source.name]
 
             return vim_item
         end,

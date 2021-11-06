fb96a2f9ebc3c08d6842d27b9348d00f3e5c5a95
Edwin Chen <edwin.chen@aquariux.com>
2021-11-07 07:23:42 +0800

    feat: Add new comment integration instructions

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,9 +5,19 @@ require("Comment").setup({
             return
         end
 
-        local type = ctx.ctype == require("Comment.utils").ctype.line and "__default" or "__multiline"
+        local comment_utils = require("Comment.utils")
+        local type = ctx.ctype == comment_utils.ctype.line and "__default" or "__multiline"
+
+        local location
+        if ctx.ctype == comment_utils.ctype.block then
+            location = require("ts_context_commentstring.utils").get_cursor_location()
+        elseif ctx.cmotion == comment_utils.cmotion.v or ctx.cmotion == comment_utils.cmotion.V then
+            location = require("ts_context_commentstring.utils").get_visual_start_location()
+        end
+
         return require("ts_context_commentstring.internal").calculate_commentstring({
             key = type,
+            location = location,
         })
     end,
 })

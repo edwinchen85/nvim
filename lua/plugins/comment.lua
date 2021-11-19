5e5d00fedb9db7c3708cd4ef6cbad561569ef071
Edwin Chen <edwin.chen@aquariux.com>
2021-11-20 05:26:28 +0800

    feat: Update Comment.nvim config

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,5 +1,48 @@
 require("Comment").setup({
-    -- integrate with nvim-ts-context-commentstring
+    active = true,
+    on_config_done = nil,
+
+    ---Add a space b/w comment and the line
+    ---@type boolean
+    padding = true,
+
+    ---Lines to be ignored while comment/uncomment.
+    ---Could be a regex string or a function that returns a regex string.
+    ---Example: Use '^$' to ignore empty lines
+    ---@type string|function
+    ignore = "^$",
+
+    ---Whether to create basic (operator-pending) and extra mappings for NORMAL/VISUAL mode
+    ---@type table
+    mappings = {
+      ---operator-pending mapping
+      ---Includes `gcc`, `gcb`, `gc[count]{motion}` and `gb[count]{motion}`
+      basic = true,
+      ---extended mapping
+      ---Includes `g>`, `g<`, `g>[count]{motion}` and `g<[count]{motion}`
+      extra = false,
+    },
+
+    ---LHS of line and block comment toggle mapping in NORMAL/VISUAL mode
+    ---@type table
+    toggler = {
+      ---line-comment toggle
+      line = "gcc",
+      ---block-comment toggle
+      block = "gbc",
+    },
+
+    ---LHS of line and block comment operator-mode mapping in NORMAL/VISUAL mode
+    ---@type table
+    opleader = {
+      ---line-comment opfunc mapping
+      line = "gc",
+      ---block-comment opfunc mapping
+      block = "gb",
+    },
+
+    ---Pre-hook, called before commenting the line
+    ---@type function|nil
     pre_hook = function(ctx)
         if not vim.tbl_contains({ "typescript", "typescriptreact" }, vim.bo.ft) then
             return
@@ -20,4 +63,8 @@ require("Comment").setup({
             location = location,
         })
     end,
+
+    ---Post-hook, called after commenting is done
+    ---@type function|nil
+    post_hook = nil,
 })

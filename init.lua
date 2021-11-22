a36835d07504ac21ae15af469869f2e2ad8941ff
Edwin Chen <edwin.chen@aquariux.com>
2021-11-22 12:03:15 +0800

    feat: Disable more vim builtin plugins

--- init.lua
+++ init.lua
@@ -63,7 +63,11 @@ vim.g.loaded_shada_plugin      = 1
 vim.g.loaded_spellfile_plugin  = 1
 vim.g.loaded_netrw             = 1
 vim.g.loaded_netrwPlugin       = 1
+vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_vimballPlugin     = 1
+vim.g.loaded_getscriptPlugin   = 1
 vim.g.loaded_remote_plugins    = 1
 
 -- the only way I've found to make this persistent

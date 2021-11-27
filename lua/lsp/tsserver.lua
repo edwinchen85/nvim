c32b4d61d36f9df68ae25a26fa311bc29aa21f62
Edwin Chen <edwin.chen@aquariux.com>
2021-11-27 19:03:54 +0800

    refactor: Remove inlay hint
    This reverts commit c0d65d20b29362e8f8a2a289b5566f68c44caf33.

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -12,16 +12,14 @@ local ts_utils_settings = {
 
 local M = {}
 M.setup = function(on_attach, capabilities)
-    local ts_utils = require("nvim-lsp-ts-utils")
-
     lspconfig.tsserver.setup({
-        init_options = ts_utils.init_options,
         on_attach = function(client, bufnr)
             client.resolved_capabilities.document_formatting = false
             client.resolved_capabilities.document_range_formatting = false
 
             on_attach(client, bufnr)
 
+            local ts_utils = require("nvim-lsp-ts-utils")
             ts_utils.setup(ts_utils_settings)
             ts_utils.setup_client(client)
 

2cd07212bf1c9be2bf96cdef6604ab9ea4579e59
Edwin Chen <edwin.chen@aquariux.com>
2021-12-10 10:26:57 +0800

    refactor: Install treesitter parsers with maintainers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,24 +1,11 @@
 require("nvim-treesitter.configs").setup({
+    ensure_installed = "maintained",
     indent = { enable = true },
     context_commentstring = {
         enable = true,
         enable_autocmd = false,
     },
     autopairs = { enable = true },
-    ensure_installed = {
-        "bash",
-        "hjson",
-        "javascript",
-        "tsx",
-        "lua",
-        "json",
-        "jsonc",
-        "python",
-        "toml",
-        "tsx",
-        "typescript",
-        "yaml",
-    },
     highlight = { enable = true },
     autotag = { enable = true },
     matchup = {

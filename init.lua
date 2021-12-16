aee5b7550f5ad3262615cc21e237bc8664210c9b
Edwin Chen <edwin.chen@aquariux.com>
2021-12-17 07:41:38 +0800

    refactor: Format nvim init luafile

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.opt.autoread = true -- Read again when file has changed outside of vim
 vim.opt.backspace = { "indent", "eol", "start" }
 vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "9999" -- Hide color column
-vim.opt.encoding = 'utf-8' -- Set default encoding to UTF-8
+vim.opt.encoding = "utf-8" -- Set default encoding to UTF-8
 vim.opt.expandtab = true -- Uses spaces instead of tabs
 vim.opt.foldenable = true
 vim.opt.foldmethod = "indent"
@@ -53,24 +53,24 @@ vim.opt.signcolumn = "yes:1" -- Always show signcolumn
 vim.opt.backupcopy = "yes"
 
 -- Disable various builtin plugins in Vim that bog down speed
-vim.g.loaded_matchparen        = 1
-vim.g.loaded_matchit           = 1
-vim.g.loaded_logiPat           = 1
-vim.g.loaded_rrhelper          = 1
-vim.g.loaded_tarPlugin         = 1
-vim.g.loaded_gzip              = 1
-vim.g.loaded_zipPlugin         = 1
-vim.g.loaded_2html_plugin      = 1
-vim.g.loaded_shada_plugin      = 1
-vim.g.loaded_spellfile_plugin  = 1
-vim.g.loaded_netrw             = 1
-vim.g.loaded_netrwPlugin       = 1
-vim.g.loaded_netrwSettings     = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
 vim.g.loaded_netrwFileHandlers = 1
 vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_vimballPlugin     = 1
-vim.g.loaded_getscriptPlugin   = 1
-vim.g.loaded_remote_plugins    = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_remote_plugins = 1
 
 vim.cmd([[
     au VimEnter * highlight VertSplit guibg=bg guifg=bg
@@ -84,14 +84,14 @@ vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro indentkeys-=:")
 -- Use spelling for markdown files ]s to find next, [s for previous, z= for suggestions when on one.
 -- Source: http:--thejakeharding.com/tutorial/2012/06/13/using-spell-check-in-vim.html
 vim.api.nvim_exec(
-  [[
+    [[
 augroup markdownSpell
     autocmd!
     autocmd FileType markdown,md,txt setlocal spell
     autocmd BufRead,BufNewFile *.md,*.txt,*.markdown setlocal spell
 augroup END
 ]],
-  false
+    false
 )
 
 -- Initialize global object for config

d18f2b68e0af5950f9ac23d4305e61f42fd5d66a
Edwin Chen <edwin.chen@aquariux.com>
2021-12-17 08:12:33 +0800

    chore: Use tokyonight theme

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,6 +2,5 @@ vim.g.tokyonight_style = "night" -- 'storm, night, day'
 vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_terminal_colors = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_colors = {hint = "orange", error = "#ff0000"}
--- vim.cmd("colorscheme tokyonight")
-vim.cmd("colorscheme solarized-flat")
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+vim.cmd("colorscheme tokyonight")

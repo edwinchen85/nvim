1801c3297fbca28a31eda8162384faf2d276e259
Edwin Chen <edwin.chen@aquariux.com>
2021-12-27 07:23:55 +0800

    feat: Close vimdows with q

--- lua/settings.lua
+++ lua/settings.lua
@@ -5,3 +5,7 @@ vim.cmd([[filetype plugin indent on]])
 
 -- the only way I've found to make this persistent
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro indentkeys-=:")
+
+-- vimdows to close with 'q'
+vim.cmd([[autocmd FileType help,qf,fugitiveblame,netrw nnoremap <buffer><silent> q :close<CR>]])
+vim.cmd([[autocmd FileType fugitive nmap <buffer><silent> q gq]])

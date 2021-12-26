33cd78f3a05624081fab63eb18ba65972be847db
Edwin Chen <edwin.chen@aquariux.com>
2021-12-27 07:51:56 +0800

    feat: Add kanagawa theme and config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -102,6 +102,7 @@ return require("packer").startup(function()
     use("folke/tokyonight.nvim")
     use("kyazdani42/nvim-web-devicons")
     use_with_config("ishan9299/nvim-solarized-lua", "solarized")
+    use_with_config("rebelot/kanagawa.nvim", "kanagawa")
     use_with_config("glepnir/dashboard-nvim", "dashboard")
     use_with_config("nvim-lualine/lualine.nvim", "lualine") -- statusline and tabline
     use_with_config("norcalli/nvim-colorizer.lua", "colorizer")

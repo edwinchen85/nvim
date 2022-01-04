d120bcf7fa0e345863978291950244b8842c12cb
Edwin Chen <edwin.chen@aquariux.com>
2022-01-04 13:03:53 +0800

    refactor: Format autopairs config luafile

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,48 +1,51 @@
 local npairs = require("nvim-autopairs")
-local Rule = require('nvim-autopairs.rule')
+local Rule = require("nvim-autopairs.rule")
 
 require("nvim-autopairs").setup({
     check_ts = true,
     map_c_w = true,
 })
 
-require('nvim-treesitter.configs').setup {
-    autopairs = {enable = true}
-}
+require("nvim-treesitter.configs").setup({
+    autopairs = { enable = true },
+})
 
-local ts_conds = require('nvim-autopairs.ts-conds')
+local ts_conds = require("nvim-autopairs.ts-conds")
 
 -- press % => %% is only inside comment or string
 npairs.add_rules({
-  Rule("%", "%", "lua")
-    :with_pair(ts_conds.is_ts_node({'string','comment'})),
-  Rule("$", "$", "lua")
-    :with_pair(ts_conds.is_not_ts_node({'function'}))
+    Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+    Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
 })
 
 -- Add spaces between parentheses
-npairs.add_rules {
-  Rule(' ', ' ')
-  :with_pair(function (opts)
-    local pair = opts.line:sub(opts.col - 1, opts.col)
-    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
-  end),
-Rule('( ', ' )')
-    :with_pair(function() return false end)
-    :with_move(function(opts)
-        return opts.prev_char:match('.%)') ~= nil
-    end)
-    :use_key(')'),
-Rule('{ ', ' }')
-    :with_pair(function() return false end)
-    :with_move(function(opts)
-        return opts.prev_char:match('.%}') ~= nil
-    end)
-    :use_key('}'),
-Rule('[ ', ' ]')
-    :with_pair(function() return false end)
-    :with_move(function(opts)
-        return opts.prev_char:match('.%]') ~= nil
-    end)
-    :use_key(']')
-}
+npairs.add_rules({
+    Rule(" ", " "):with_pair(function(opts)
+        local pair = opts.line:sub(opts.col - 1, opts.col)
+        return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+    end),
+    Rule("( ", " )")
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match(".%)") ~= nil
+        end)
+        :use_key(")"),
+    Rule("{ ", " }")
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match(".%}") ~= nil
+        end)
+        :use_key("}"),
+    Rule("[ ", " ]")
+        :with_pair(function()
+            return false
+        end)
+        :with_move(function(opts)
+            return opts.prev_char:match(".%]") ~= nil
+        end)
+        :use_key("]"),
+})

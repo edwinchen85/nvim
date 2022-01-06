6a265947001010b485a5c2d2b738b9c4b5de0333
Edwin Chen <edwin.chen@aquariux.com>
2022-01-07 07:35:06 +0800

    refactor(theme): Add try catch in colorscheme

--- lua/theme.lua
+++ lua/theme.lua
@@ -3,4 +3,11 @@ vim.g.tokyonight_italic_functions = true
 vim.g.tokyonight_terminal_colors = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-vim.cmd("colorscheme kanagawa")
+vim.cmd([[
+  try
+    colorscheme kanagawa
+  catch /^Vim\%((\a\+)\)\=:E185/
+    colorscheme default
+    set background=dark
+  endtry
+]])

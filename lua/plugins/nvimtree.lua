b4479c4580e27c852a0c18c82d3ce0581bd148ae
Edwin Chen <edwin.chen@aquariux.com>
2022-01-14 07:58:41 +0800

    refactor(nvimtree): Show dotfiles

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -59,7 +59,7 @@ require("nvim-tree").setup({
         args = {},
     },
     filters = {
-        dotfiles = true,
+        dotfiles = false,
         custom = { ".git", "node_modules", ".cache", "tags", ".DS_Store" },
     },
     git = {

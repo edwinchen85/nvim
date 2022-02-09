e5e87ed1aa899efa127c0e0fe1d5f2105ebd09a3
Edwin Chen <edwin.chen@aquariux.com>
2022-02-09 12:08:26 +0800

    feat(telescope): Add delete buffer action

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -90,11 +90,13 @@ telescope.setup({
                 ["<C-k>"] = actions.move_selection_previous,
                 ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
                 ["<CR>"] = actions.select_default + actions.center,
+                ["<c-d>"] = require("telescope.actions").delete_buffer,
             },
             n = {
                 ["<C-j>"] = actions.move_selection_next,
                 ["<C-k>"] = actions.move_selection_previous,
                 ["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+                ["<c-d>"] = require("telescope.actions").delete_buffer,
             },
         },
     },

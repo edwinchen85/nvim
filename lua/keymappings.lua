45c32b8219e2ddbb0a91aa64f600978562268608
Edwin Chen <edwin.chen@aquariux.com>
2022-02-09 12:23:37 +0800

    refactor(keymap): Use visual mode only to move lines

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -66,10 +66,8 @@ u.nmap("<C-l>", "<C-w>l")
 u.nmap("<C-c>", "<C-w>c")
 
 -- Move selected line / block of text
-u.nmap("<M-j>", ":m .+1<CR>==")
-u.nmap("<M-k>", ":m .-2<CR>==")
-u.xmap("<M-j>", ":m '>+1<CR>gv=gv")
-u.xmap("<M-k>", ":m '<-2<CR>gv=gv")
+u.xmap("<C-j>", ":m '>+1<CR>gv=gv")
+u.xmap("<C-k>", ":m '<-2<CR>gv=gv")
 
 -- Resize window
 u.nmap("<C-Up>", ":resize +2<CR>")

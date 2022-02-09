498a057bbafcc8f3d2e4303ccc2727db834b5e48
Edwin Chen <edwin.chen@aquariux.com>
2022-02-09 12:11:59 +0800

    feat(theme): Add transparent background

--- lua/theme.lua
+++ lua/theme.lua
@@ -1,8 +1,13 @@
 vim.g.tokyonight_style = "night" -- 'storm, night, day'
+vim.g.tokyonight_transparent = true
+vim.g.tokyonight_transparent_sidebar = true
+vim.g.tokyonight_hide_inactive_statusline = true
 vim.g.tokyonight_italic_functions = true
+vim.g.tokyonight_italic_variables = true
 vim.g.tokyonight_terminal_colors = true
 vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
 vim.cmd([[
   try
     colorscheme tokyonight
@@ -10,4 +15,12 @@ vim.cmd([[
     colorscheme default
     set background=dark
   endtry
+]])
+
+vim.cmd([[
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NonText guibg=NONE ctermbg=NONE
+  hi SignColumn guibg=NONE ctermbg=NONE
+  hi LineNr guibg=NONE ctermbg=NONE
+  hi StatusLine guibg=NONE ctermbg=NONE
 ]])

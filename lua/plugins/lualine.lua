24aaf2be63ecfee1c700c68188a2785f377b1c86
Edwin Chen <edwin.chen@aquariux.com>
2022-02-18 05:20:28 +0800

    feat(lualine): Customise settings

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,51 @@
+local function theme()
+    local colors = {
+        darkgray = "#16161d",
+        gray = "#727169",
+        innerbg = nil,
+        outerbg = "#16161D",
+        gutter = "#3b4261",
+        normal = "#7aa2f7",
+        insert = "#9ece6a",
+        visual = "#bb9af7",
+        replace = "#f7768e",
+        command = "#e0af68",
+    }
+
+    return {
+        inactive = {
+            a = { fg = colors.gray, bg = colors.innerbg },
+            b = { fg = colors.gray, bg = colors.innerbg },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        visual = {
+            a = { fg = colors.darkgray, bg = colors.visual },
+            b = { fg = colors.visual, bg = colors.gutter },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        replace = {
+            a = { fg = colors.darkgray, bg = colors.replace },
+            b = { fg = colors.replace, bg = colors.gutter },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        normal = {
+            a = { fg = colors.darkgray, bg = colors.normal },
+            b = { fg = colors.normal, bg = colors.gutter },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        insert = {
+            a = { fg = colors.darkgray, bg = colors.insert },
+            b = { fg = colors.insert, bg = colors.gutter },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+        command = {
+            a = { fg = colors.darkgray, bg = colors.command },
+            b = { fg = colors.command, bg = colors.gutter },
+            c = { fg = colors.gray, bg = colors.innerbg },
+        },
+    }
+end
+
 local function diff_source()
     local gitsigns = vim.b.gitsigns_status_dict
     if gitsigns then
@@ -31,15 +79,15 @@ vim.cmd([[autocmd User LspProgressUpdate let &ro = &ro]])
 
 local config = {
     options = {
-        theme = "auto",
+        theme = theme(),
         section_separators = { left = "", right = "" },
         component_separators = { left = "", right = "" },
-        disabled_filetypes = { "NvimTree" },
+        disabled_filetypes = { "alpha", "NvimTree" },
         always_divide_middle = false,
         icons_enabled = true,
     },
     sections = {
-        lualine_a = { "mode" },
+        lualine_a = { { "filename", path = 1, symbols = { modified = " [+]", readonly = " [RO]" } } },
         lualine_b = {
             { "b:gitsigns_head", icon = "" },
         },
@@ -50,7 +98,6 @@ local config = {
                 sources = { "nvim_diagnostic" },
                 symbols = { error = "E ", warn = "W ", info = "I ", hint = "H " },
             },
-            { "filename", path = 1, symbols = { modified = " [+]", readonly = " [RO]" } },
         },
         lualine_x = { "filetype", lsp_progress },
         lualine_y = { "progress" },
@@ -69,11 +116,11 @@ local config = {
 local M = {}
 
 function M.load()
-    local name = vim.g.colors_name or ""
-    local ok, _ = pcall(require, "lualine.themes." .. name)
-    if ok then
-        config.options.theme = name
-    end
+    -- local name = vim.g.colors_name or ""
+    -- local ok, _ = pcall(require, "lualine.themes." .. name)
+    -- if ok then
+    --     config.options.theme = name
+    -- end
     require("lualine").setup(config)
 end
 

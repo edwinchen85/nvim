ad396d2db57b4bb88d28e590ef87a0d75d30ddfd
Edwin Chen <edwin.chen@aquariux.com>
2022-02-21 08:16:27 +0800

    refactor(toggleterm): Format luafile

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,12 @@
-require("toggleterm").setup{
-  size = vim.o.columns * 0.4,
-  open_mapping = [[<c-\>]],
-  hide_numbers = true, -- hide the number column in toggleterm buffers
-  shade_terminals = false,
-  start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
-  persist_size = true,
-  direction = 'vertical',
-  close_on_exit = true, -- close the terminal window when the process exits
-  shell = vim.o.shell, -- change the default shell
-}
+require("toggleterm").setup({
+    size = vim.o.columns * 0.4,
+    open_mapping = [[<c-\>]],
+    hide_numbers = true, -- hide the number column in toggleterm buffers
+    shade_terminals = false,
+    start_in_insert = true,
+    insert_mappings = true, -- whether or not the open mapping applies in insert mode
+    persist_size = true,
+    direction = "vertical",
+    close_on_exit = true, -- close the terminal window when the process exits
+    shell = vim.o.shell, -- change the default shell
+})

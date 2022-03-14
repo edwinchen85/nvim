464ad2010ad7cc14e0e0f64826ae4940476c7d83
Edwin Chen <edwin.chen@aquariux.com>
2022-03-14 14:00:51 +0800

    refactor(plugin): Remove friendly-snippets plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -93,7 +93,6 @@ return require("packer").startup(function(use)
 
     -- snippets
     use("L3MON4D3/LuaSnip") --snippet engine
-    use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
     use({
         "windwp/nvim-autopairs", -- autocomplete pairs

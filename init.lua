d0ce55022b1d19922b7c05f3ab657f96cf74528b
Edwin Chen <edwin.chen@aquariux.com>
2022-03-27 20:09:41 +0800

    refactor(setting): Relocate markdown spelling setting

--- init.lua
+++ init.lua
@@ -79,19 +79,6 @@ vim.g.loaded_vimballPlugin = 1
 vim.g.loaded_getscriptPlugin = 1
 vim.g.loaded_remote_plugins = 1
 
--- Use spelling for markdown files ]s to find next, [s for previous, z= for suggestions when on one.
--- Source: http:--thejakeharding.com/tutorial/2012/06/13/using-spell-check-in-vim.html
-vim.api.nvim_exec(
-    [[
-augroup markdownSpell
-    autocmd!
-    autocmd FileType markdown,md,txt setlocal spell
-    autocmd BufRead,BufNewFile *.md,*.txt,*.markdown setlocal spell
-augroup END
-]],
-    false
-)
-
 -- Initialize global object for config
 global = {}
 
--- lua/settings.lua
+++ lua/settings.lua
@@ -1,3 +1,16 @@
+-- Use spelling for markdown files ]s to find next, [s for previous, z= for suggestions when on one.
+-- Source: http:--thejakeharding.com/tutorial/2012/06/13/using-spell-check-in-vim.html
+vim.api.nvim_exec(
+    [[
+augroup markdownSpell
+autocmd!
+autocmd FileType markdown,md,txt setlocal spell
+autocmd BufRead,BufNewFile *.md,*.txt,*.markdown setlocal spell
+augroup END
+]],
+    false
+)
+
 -- hide vertical split
 vim.cmd([[augroup VimEnter * highlight VertSplit guibg=bg guifg=bg]])
 

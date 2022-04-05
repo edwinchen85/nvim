59d82e750e33c63f19c4d362d6fe4387a0802cb1
Edwin Chen <edwin.chen@aquariux.com>
2022-04-05 16:20:03 +0800

    feat(lsp): Allow focus on omnifocus with <C-w><C-w>

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -5,7 +5,7 @@ local api = vim.api
 
 local border_opts = {
     border = "rounded",
-    focusable = false,
+    focusable = true,
     style = "minimal",
     source = "always",
     scope = "line",

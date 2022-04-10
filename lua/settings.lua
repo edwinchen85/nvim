2ceb85f7a98a549e11a7fb8b1d414b8632172d39
Edwin Chen <edwin.chen@aquariux.com>
2022-04-10 15:43:22 +0800

    refactor(setting): Remove redundant setting command

--- lua/settings.lua
+++ lua/settings.lua
@@ -14,7 +14,7 @@ augroup END
 -- hide vertical split
 vim.cmd([[augroup VimEnter * highlight VertSplit guibg=bg guifg=bg]])
 
-vim.cmd([[filetype plugin indent on]])
+-- vim.cmd([[filetype plugin indent on]])
 
 -- the only way I've found to make this persistent
 vim.cmd("autocmd BufEnter * setlocal formatoptions-=cro indentkeys-=:")

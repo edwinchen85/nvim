f9bbea2e3cee59cf99980deccfe2de7a61549442
Edwin Chen <edwin.chen@aquariux.com>
2022-04-11 10:50:29 +0800

    refactor(treesitter): Replace deprecated ensure installed value with all

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = "maintained",
+    ensure_installed = "all",
     indent = { enable = true },
     context_commentstring = {
         enable = true,

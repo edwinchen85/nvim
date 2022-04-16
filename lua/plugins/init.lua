b23966c36a85c62d0e6426cc210fd7f2ec54b568
Edwin Chen <edwin.chen@aquariux.com>
2022-04-16 21:53:01 +0800

    refactor(plugin): Remove cmp-cmdline

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -82,7 +82,6 @@ return require("packer").startup(function(use)
             "hrsh7th/cmp-nvim-lsp",
             "hrsh7th/cmp-nvim-lua",
             "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-cmdline",
             "hrsh7th/cmp-path",
             "folke/lua-dev.nvim",
             "saadparwaiz1/cmp_luasnip", -- snippet completions

63007507f9cc0f5938b4c8e51dd5eed1b67cb330
Edwin Chen <edwin.chen@aquariux.com>
2022-04-16 14:31:44 +0800

    refactor(dashboard): Remove unused shortcuts

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -23,8 +23,6 @@ dashboard.section.buttons.val = {
     dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
     dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
     dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-    dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
     dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
     dashboard.button("q", "  Quit dashboard", ":Alpha <CR>"),
 }

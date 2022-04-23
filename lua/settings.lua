efcbeae2bbddc1d36ef8cd3deee0c05234248896
Edwin Chen <edwin.chen@aquariux.com>
2022-04-23 16:24:59 +0800

    refactor(setting): Default WinSeparator achieve same effect

--- lua/settings.lua
+++ lua/settings.lua
@@ -11,9 +11,6 @@ augroup END
     false
 )
 
--- hide vertical split
-vim.cmd([[augroup VimEnter * highlight VertSplit guibg=bg guifg=bg]])
-
 -- vim.cmd([[filetype plugin indent on]])
 
 -- the only way I've found to make this persistent

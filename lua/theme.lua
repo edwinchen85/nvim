db6becc0366b61975340a79d31e10ee1c7a5b736
Edwin Chen <edwin.chen@aquariux.com>
2022-06-25 20:15:31 +0800

    refactor(theme): Disable transparency

--- lua/theme.lua
+++ lua/theme.lua
@@ -22,18 +22,18 @@ vim.cmd([[
   endtry
 ]])
 
-vim.cmd([[
-  hi Folded guibg=NONE ctermbg=NONE
-  hi Normal guibg=NONE ctermbg=NONE
-  hi NormalFloat guibg=NONE ctermbg=NONE
-  hi NonText guibg=NONE ctermbg=NONE
-  hi SignColumn guibg=NONE ctermbg=NONE
-  hi LineNr guibg=NONE ctermbg=NONE
-  hi StatusLine guibg=NONE ctermbg=NONE
-  hi TelescopeNormal guibg=NONE ctermbg=NONE
-  hi TelescopeBorder guibg=NONE ctermbg=NONE
-  hi GitSignsAdd guibg=NONE ctermbg=NONE
-  hi GitSignsChange guibg=NONE ctermbg=NONE
-  hi GitSignsDelete guibg=NONE ctermbg=NONE
-  hi WhichKeyFloat guibg=NONE ctermbg=NONE
-]])
+-- vim.cmd([[
+--   hi Folded guibg=NONE ctermbg=NONE
+--   hi Normal guibg=NONE ctermbg=NONE
+--   hi NormalFloat guibg=NONE ctermbg=NONE
+--   hi NonText guibg=NONE ctermbg=NONE
+--   hi SignColumn guibg=NONE ctermbg=NONE
+--   hi LineNr guibg=NONE ctermbg=NONE
+--   hi StatusLine guibg=NONE ctermbg=NONE
+--   hi TelescopeNormal guibg=NONE ctermbg=NONE
+--   hi TelescopeBorder guibg=NONE ctermbg=NONE
+--   hi GitSignsAdd guibg=NONE ctermbg=NONE
+--   hi GitSignsChange guibg=NONE ctermbg=NONE
+--   hi GitSignsDelete guibg=NONE ctermbg=NONE
+--   hi WhichKeyFloat guibg=NONE ctermbg=NONE
+-- ]])

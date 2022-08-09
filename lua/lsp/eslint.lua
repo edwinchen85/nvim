13361398ee7bb489fb134fe79dedcb8c6ba6d71d
Edwin Chen <edwin.chen@aquariux.com>
2022-08-09 16:19:34 +0800

    feat(eslint): Handle eslint error

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -14,6 +14,17 @@ local M = {
                     enable = true,
                 },
             },
+            handlers = {
+                -- this error shows up occasionally when formatting
+                -- formatting actually works, so this will supress it
+                ["window/showMessageRequest"] = function(_, result)
+                    if result.message:find("ENOENT") then
+                        return vim.NIL
+                    end
+
+                    return vim.lsp.handlers["window/showMessageRequest"](nil, result)
+                end,
+            },
         })
     end,
 }

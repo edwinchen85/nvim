c41d2a70e83243cc5c58300ae166f52c17a07d79
Edwin Chen <edwin.chen@aquariux.com>
2022-08-09 16:20:08 +0800

    feat(eslint): Add .eslintrc.json in root_dir

--- lua/lsp/eslint.lua
+++ lua/lsp/eslint.lua
@@ -3,7 +3,7 @@ local M = {
         local lspconfig = require("lspconfig")
 
         lspconfig["eslint"].setup({
-            root_dir = lspconfig.util.root_pattern(".eslintrc", ".eslintrc.js"),
+            root_dir = lspconfig.util.root_pattern(".eslintrc", ".eslintrc.js", ".eslintrc.json"),
             on_attach = function(client, bufnr)
                 client.resolved_capabilities.document_formatting = true
                 on_attach(client, bufnr)

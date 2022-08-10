3ed2a463a32da34ebd324c3220beaa1c998c2abf
Edwin Chen <edwin.chen@aquariux.com>
2022-08-10 21:10:56 +0800

    feat(lsp): Show diagnostic signs in gutter

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -13,7 +13,24 @@ local border_opts = {
     prefix = "",
 }
 
-vim.diagnostic.config({ virtual_text = false, float = border_opts })
+local signs = {
+    { name = "DiagnosticSignError", text = "" },
+    { name = "DiagnosticSignWarn", text = "" },
+    { name = "DiagnosticSignHint", text = "" },
+    { name = "DiagnosticSignInfo", text = "" },
+}
+
+for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+end
+
+vim.diagnostic.config({
+    virtual_text = false,
+    float = border_opts,
+    signs = {
+        active = signs,
+    },
+})
 
 lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, border_opts)
 lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, border_opts)

6af13b96df80d931f6e84beb2715b4db06cb9969
Edwin Chen <edwin.chen@aquariux.com>
2022-09-15 11:14:27 +0800

    feat(sumneko): Update lua-dev setup

--- lua/lsp/sumneko_lua.lua
+++ lua/lsp/sumneko_lua.lua
@@ -28,17 +28,23 @@ local settings = {
 local M = {}
 
 M.setup = function(on_attach, capabilities)
-    local luadev = require("lua-dev").setup({
-        lspconfig = {
-            on_attach = on_attach,
-            settings = settings,
-            flags = {
-                debounce_text_changes = 150,
-            },
-            capabilities = capabilities,
-        },
+    require("lua-dev").setup({
+        override = function(root_dir, library)
+            if root_dir:match("dotfiles") then
+                library.enabled = true
+                library.plugins = true
+            end
+        end,
     })
-    require("lspconfig").sumneko_lua.setup(luadev)
+
+    require("lspconfig").sumneko_lua.setup({
+        on_attach = on_attach,
+        settings = settings,
+        flags = {
+            debounce_text_changes = 150,
+        },
+        capabilities = capabilities,
+    })
 end
 
 return M

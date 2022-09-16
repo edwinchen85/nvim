39e4e7392007b0d434813d001cb96672db50d8ff
Edwin Chen <edwin.chen@aquariux.com>
2022-09-16 15:30:31 +0800

    feat(whichkey): Align center and increase column gap spacing

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -27,9 +27,10 @@ require("which-key").setup({
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
     },
     layout = {
+        align = "center",
         height = { min = 4, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
+        spacing = 10, -- spacing between columns
     },
     ignore_missing = true, -- enable this to hide mappings for which you didn't specify a label
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate

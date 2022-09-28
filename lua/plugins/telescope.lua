4ef7b6ed85eec3810c6e34732b03e757342f86ca
Edwin Chen <edwinchen85@gmail.com>
2022-09-28 09:18:27 +0800

    style(telescope): Format lua file

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 local status_ok, telescope = pcall(require, "telescope")
 if not status_ok then
-  return
+    return
 end
 
 local builtin = require("telescope.builtin")
@@ -14,138 +14,138 @@ local commands = require("config.commands")
 local api = vim.api
 
 telescope.setup({
-  extensions = {
-    fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true },
-    ["ui-select"] = { require("telescope.themes").get_cursor({}) },
-  },
-  defaults = {
-    preview = {
-      timeout = 500,
+    extensions = {
+        fzf = { fuzzy = true, override_generic_sorter = true, override_file_sorter = true },
+        ["ui-select"] = { require("telescope.themes").get_cursor({}) },
     },
-    file_browser = {
-      hidden = true,
-    },
-    find_command = { "rg", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
-    vimgrep_arguments = {
-      "rg",
-      "--color=never",
-      "--no-heading",
-      "--with-filename",
-      "--line-number",
-      "--column",
-      "--smart-case",
-      "--ignore",
-      "--hidden",
-      "-g",
-      "!.git",
-    },
-    prompt_prefix = " ",
-    selection_caret = " ",
-    entry_prefix = "  ",
-    multi_icon = " ",
-    initial_mode = "insert",
-    selection_strategy = "reset",
-    sorting_strategy = "ascending",
-    layout_strategy = "vertical",
-    layout_config = {
-      prompt_position = "top",
-      horizontal = {
-        width_padding = 0.04,
-        height_padding = 0.1,
-        preview_width = 0.4,
-      },
-      vertical = {
-        width_padding = 0.05,
-        height_padding = 1,
-        preview_height = 0.5,
-      },
-    },
-    file_sorter = require("telescope.sorters").get_fzy_sorter,
-    file_ignore_patterns = {
-      "node_modules/**/*.d.ts",
-      "node_modules/**.*",
-      "%-lock.json",
-      "%-lock.yaml",
-      "yarn.lock",
-      ".git/**",
-    },
-    generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
-    dynamic_preview_title = true,
-    path_display = {},
-    winblend = 0,
-    border = {},
-    borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
-    color_devicons = true,
-    use_less = true,
-    set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
-    file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-    grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-    qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+    defaults = {
+        preview = {
+            timeout = 500,
+        },
+        file_browser = {
+            hidden = true,
+        },
+        find_command = { "rg", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case" },
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--ignore",
+            "--hidden",
+            "-g",
+            "!.git",
+        },
+        prompt_prefix = " ",
+        selection_caret = " ",
+        entry_prefix = "  ",
+        multi_icon = " ",
+        initial_mode = "insert",
+        selection_strategy = "reset",
+        sorting_strategy = "ascending",
+        layout_strategy = "vertical",
+        layout_config = {
+            prompt_position = "top",
+            horizontal = {
+                width_padding = 0.04,
+                height_padding = 0.1,
+                preview_width = 0.4,
+            },
+            vertical = {
+                width_padding = 0.05,
+                height_padding = 1,
+                preview_height = 0.5,
+            },
+        },
+        file_sorter = require("telescope.sorters").get_fzy_sorter,
+        file_ignore_patterns = {
+            "node_modules/**/*.d.ts",
+            "node_modules/**.*",
+            "%-lock.json",
+            "%-lock.yaml",
+            "yarn.lock",
+            ".git/**",
+        },
+        generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+        dynamic_preview_title = true,
+        path_display = {},
+        winblend = 0,
+        border = {},
+        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+        color_devicons = true,
+        use_less = true,
+        set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+        file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+        grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+        qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
 
-    -- Developer configurations: Not meant for general override
-    buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
-    mappings = {
-      i = {
-        ["<C-n>"] = actions.cycle_history_next,
-        ["<C-p>"] = actions.cycle_history_prev,
-        ["<C-c>"] = actions.close,
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<CR>"] = actions.select_default + actions.center,
-        ["<c-d>"] = require("telescope.actions").delete_buffer,
-        ["<C-q>"] = trouble.open_with_trouble,
-      },
-      n = {
-        ["<C-j>"] = actions.move_selection_next,
-        ["<C-k>"] = actions.move_selection_previous,
-        ["<c-d>"] = require("telescope.actions").delete_buffer,
-        ["<C-q>"] = trouble.open_with_trouble,
-      },
+        -- Developer configurations: Not meant for general override
+        buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+        mappings = {
+            i = {
+                ["<C-n>"] = actions.cycle_history_next,
+                ["<C-p>"] = actions.cycle_history_prev,
+                ["<C-c>"] = actions.close,
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<CR>"] = actions.select_default + actions.center,
+                ["<c-d>"] = require("telescope.actions").delete_buffer,
+                ["<C-q>"] = trouble.open_with_trouble,
+            },
+            n = {
+                ["<C-j>"] = actions.move_selection_next,
+                ["<C-k>"] = actions.move_selection_previous,
+                ["<c-d>"] = require("telescope.actions").delete_buffer,
+                ["<C-q>"] = trouble.open_with_trouble,
+            },
+        },
     },
-  },
 })
 
 telescope.load_extension("fzf")
 telescope.load_extension("ui-select")
 
 _G.global.telescope = {
-  -- grep string from prompt
-  grep_prompt = function()
-    builtin.grep_string({
-      shorten_path = true,
-      search = vim.fn.input("grep > "),
-    })
-  end,
+    -- grep string from prompt
+    grep_prompt = function()
+        builtin.grep_string({
+            shorten_path = true,
+            search = vim.fn.input("grep > "),
+        })
+    end,
 
-  -- try git_files and fall back to find_files
-  find_files = function()
-    local current = api.nvim_get_current_buf()
-    local opts = {
-      attach_mappings = function(_, map)
-        -- replace current buffer with selected
-        map("i", "<C-r>", function(prompt_bufnr)
-          set.edit(prompt_bufnr, "edit")
+    -- try git_files and fall back to find_files
+    find_files = function()
+        local current = api.nvim_get_current_buf()
+        local opts = {
+            attach_mappings = function(_, map)
+                -- replace current buffer with selected
+                map("i", "<C-r>", function(prompt_bufnr)
+                    set.edit(prompt_bufnr, "edit")
 
-          commands.bdelete(current)
-        end)
+                    commands.bdelete(current)
+                end)
 
-        -- edit file and matching test file in split
-        map("i", "<C-f>", function(prompt_bufnr)
-          set.edit(prompt_bufnr, "edit")
+                -- edit file and matching test file in split
+                map("i", "<C-f>", function(prompt_bufnr)
+                    set.edit(prompt_bufnr, "edit")
 
-          commands.wwipeall()
-          commands.edit_test_file("vsplit $FILE | wincmd w")
-        end)
+                    commands.wwipeall()
+                    commands.edit_test_file("vsplit $FILE | wincmd w")
+                end)
 
-        return true
-      end,
-    }
+                return true
+            end,
+        }
 
-    local is_git_project = pcall(builtin.git_files, opts)
-    if not is_git_project then
-      builtin.find_files(opts)
-    end
-  end,
+        local is_git_project = pcall(builtin.git_files, opts)
+        if not is_git_project then
+            builtin.find_files(opts)
+        end
+    end,
 }
 
 -- lsp

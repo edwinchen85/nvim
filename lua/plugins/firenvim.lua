40b53aa17728aee85751de18103bb9ce3431ecb7
Edwin Chen <edwinchen85@gmail.com>
2022-10-15 13:13:14 +0800

    feat(firenvim): Show mode and hide side column

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -27,6 +27,8 @@ vim.cmd([[
   function! OnUIEnter(event) abort
     if 'Firenvim' ==# get(get(nvim_get_chan_info(a:event.chan), 'client', {}), 'name', '')
       set laststatus=0
+      set showmode
+      set signcolumn=no
     endif
   endfunction
 

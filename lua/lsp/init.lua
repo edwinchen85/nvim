14b2d3e5fc32737a07270df36ca33e81fcb6e8f2
Edwin Chen <edwinchen85@gmail.com>
2022-11-15 09:23:35 +0800

    fix: Replace deprecated update_capabilities with default_capabilities

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -130,7 +130,7 @@ local on_attach = function(client, bufnr)
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 for _, server in ipairs({
     "bashls",

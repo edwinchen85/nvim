4b79ac1c3c25cb5bce3408d729430d4ebcaf2bc9
Edwin Chen <edwinchen85@gmail.com>
2022-11-15 09:49:29 +0800

    feat: Disable javascript or typescript suggestion actions

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -8,6 +8,11 @@ M.setup = function(on_attach, capabilities)
                 on_attach(client, bufnr)
 
                 u.buf_map(bufnr, "n", "go", ":TypescriptAddMissingImports<CR>")
+
+                require("nvim-lsp-ts-utils").setup({
+                    filter_out_diagnostics_by_code = { 80001, 80006 },
+                })
+                require("nvim-lsp-ts-utils").setup_client(client)
             end,
             capabilities = capabilities,
         },

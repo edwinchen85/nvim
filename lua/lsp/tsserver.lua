be42fb0433af33f6265b7826be63bb8d86b616e3
Edwin Chen <edwinchen85@gmail.com>
2022-11-21 15:24:27 +0800

    chore: Add reference link for diagnostic messages

--- lua/lsp/tsserver.lua
+++ lua/lsp/tsserver.lua
@@ -10,7 +10,7 @@ M.setup = function(on_attach, capabilities)
                 u.buf_map(bufnr, "n", "go", ":TypescriptAddMissingImports<CR>")
 
                 require("nvim-lsp-ts-utils").setup({
-                    filter_out_diagnostics_by_code = { 80001, 80006, 80007 },
+                    -- Refer to https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
                     filter_out_diagnostics_by_code = { 80001, 80005, 80006, 80007 },
                 })
                 require("nvim-lsp-ts-utils").setup_client(client)

f64482463c734cc520b547e42e4d00dfcfd653a9
Edwin Chen <edwinchen85@gmail.com>
2022-12-20 10:57:49 +0800

    feat(lsp): Setup tailwindcss lsp configs

--- lua/lsp/tailwindcss.lua
+++ lua/lsp/tailwindcss.lua
@@ -1,7 +1,32 @@
 local M = {}
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+capabilities.textDocument.colorProvider = { dynamicRegistration = false }
+capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true,
+}
+
 M.setup = function()
-    require("lspconfig").tailwindcss.setup({})
+    require("lspconfig").tailwindcss.setup({
+        capabilities = capabilities,
+        filetypes = { "html", "mdx", "javascript", "javascriptreact", "typescriptreact", "vue", "svelte" },
+        settings = {
+            tailwindCSS = {
+                lint = {
+                    cssConflict = "warning",
+                    invalidApply = "error",
+                    invalidConfigPath = "error",
+                    invalidScreen = "error",
+                    invalidTailwindDirective = "error",
+                    invalidVariant = "error",
+                    recommendedVariantOrder = "warning",
+                },
+                validate = true,
+            },
+        },
+    })
 end
 
 return M

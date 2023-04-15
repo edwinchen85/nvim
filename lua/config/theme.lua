e8ed6d9b7ab829765ba3c9c9a544e108f0bcf978
Edwin Chen <edwinchen85@gmail.com>
2023-04-15 17:42:04 +0800

    feat(theme): Customise telescope user interface styles

--- lua/config/theme.lua
+++ lua/config/theme.lua
@@ -9,6 +9,13 @@ vim.cmd([[
 
 vim.cmd([[
     hi LineNr guifg=#565f89
+    hi TelescopeNormal guifg=#a9b1d6 guibg=#1f2335
+    hi TelescopeBorder guifg=#1f2335 guibg=#1f2335
+    hi TelescopePromptNormal guibg=#2d3149
+    hi TelescopePromptBorder guifg=#2d3149 guibg=#2d3149
+    hi TelescopePromptTitle guifg=#2d3149 guibg=#2d3149
+    hi TelescopePreviewTitle guifg=#1f2335 guibg=#1f2335
+    hi TelescopeResultsTitle guifg=#1f2335 guibg=#1f2335
 ]])
 
 -- vim.cmd([[

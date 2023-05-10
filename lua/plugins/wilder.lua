3d2e2a36510e047fff527766682326708b2364fa
Edwin Chen <edwinchen85@gmail.com>
2023-05-10 10:22:46 +0800

    feat(wilder): Add border

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -10,13 +10,18 @@ wilder.setup({
 
 wilder.set_option(
     "renderer",
-    wilder.popupmenu_renderer({
-        -- highlighter applies highlighting to the candidates
+    wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
         highlighter = wilder.basic_highlighter(),
         highlights = {
-            accent = wilder.make_hl("WilderAccent", "Pmenu", { { a = 1 }, { a = 1 }, { foreground = "#f4468f" } }),
+            accent = wilder.make_hl(
+                "WilderAccent",
+                "Pmenu",
+                { { a = 1 }, { a = 1 }, { foreground = "#f4468f", background = "None" } }
+            ),
+            border = "None", -- highlight to use for the border
         },
-        left = { " ", wilder.popupmenu_devicons() },
-        right = { " ", wilder.popupmenu_scrollbar() },
-    })
+        -- 'single', 'double', 'rounded' or 'solid'
+        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+        border = "rounded",
+    }))
 )

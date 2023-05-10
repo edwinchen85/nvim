2b5115203a0b23c610b4fb4c6a5b38e1f23a31dd
Edwin Chen <edwinchen85@gmail.com>
2023-05-10 16:30:28 +0800

    docs(theme): Update tokyonight comment strings

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -30,8 +30,8 @@ require("tokyonight").setup({
 
     --- You can override specific highlights to use other groups or a hex color
     --- function will be called with a Highlights and ColorScheme table
-    ---@param highlights Highlights
-    ---@param colors ColorScheme
+    ---@param hl Highlights
+    ---@param c ColorScheme
     on_highlights = function(hl, c)
         local prompt = "#2d3149"
         hl.LineNr = {

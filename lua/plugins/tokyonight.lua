adbce8d429cf3dadbc83ad7900a69a765c407c10
Edwin Chen <edwinchen85@gmail.com>
2023-05-10 16:42:31 +0800

    feat(theme): Set comment color as float border

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -66,7 +66,7 @@ require("tokyonight").setup({
         }
         hl.FloatBorder = {
             bg = c.none,
-            fg = c.none,
+            fg = c.comment,
         }
         hl.NormalFloat = {
             bg = c.none,

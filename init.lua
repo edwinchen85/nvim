e080d9a624f445854fb30bb9dd27f2d5d18acbc6
Edwin Chen <edwinchen85@gmail.com>
2024-09-11 21:15:17 +0800

    feat(option): Enable smart indent

--- init.lua
+++ init.lua
@@ -30,6 +30,7 @@ vim.opt.showtabline = 0
 vim.opt.laststatus = 3 -- Global status line
 vim.opt.smartcase = true -- Do not ignore case with capitals
 vim.opt.smartindent = true -- Insert indents automatically
+vim.opt.breakindent = true -- Enable smart indent
 vim.opt.spelllang = "en"
 vim.opt.splitbelow = true -- Put new windows below current
 vim.opt.splitright = true -- Put new windows right of current

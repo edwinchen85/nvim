a8abd4a3ecfc2bb8b7ce881aa351fb77dacfe844
Edwin Chen <edwinchen85@gmail.com>
2024-09-11 21:24:00 +0800

    feat(option): Set better completion experience

--- init.lua
+++ init.lua
@@ -63,6 +63,7 @@ vim.opt.wildignore:append(".git/**")
 vim.opt.path:append("**")
 vim.opt.title = true
 vim.opt.confirm = true -- Ask for confirmation instead of erroring
+vim.opt.completeopt = { "menuone", "noselect" } -- Set completeopt to have a better completion experience
 
 -- Disable various builtin plugins in Vim that bog down speed
 vim.g.loaded_matchparen = 1

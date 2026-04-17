e13989eb3ccca76ed617f03f0e2448fb57dbbb1f
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:16:07 +0800

    feat(tokyonight): Adapt to lazyvim

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,80 +1,153 @@
-require("tokyonight").setup({
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    style = "moon", -- The theme comes in four styles, `storm`, `moon`, a darker variant `night` and `day`
-    transparent = false, -- Enable this to disable setting the background color
-    terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-    styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = { italic = true },
-        variables = { italic = true },
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark", -- style for sidebars, see below
-        floats = "dark", -- style for floating windows
-    },
-    sidebars = { "qf", "help", "terminal", "packer" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-    day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-    hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
-    dim_inactive = false, -- dims inactive windows
-    lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+return {
+    "folke/tokyonight.nvim",
+    lazy = "false",
+    priority = 1000,
+    config = function()
+        local function highlight_treesitter_context(hl, c)
+            local prompt = "#2d3149"
+            hl.TreesitterContext = {
+                bg = "NONE",
+                fg = "NONE",
+            }
+            -- hl.TreesitterContextBottom = {
+            --     bg = c.comment,
+            --     fg = c.comment,
+            -- }
+            -- hi TreesitterContextLineNumberBottom gui=underline guisp=Grey
+        end
 
-    --- You can override specific color groups to use other groups or a hex color
-    --- function will be called with a ColorScheme table
-    ---@param colors ColorScheme
-    on_colors = function(colors)
-        colors.border = "#565f89"
+        local function highlight_telescope(hl, c)
+            local prompt = c.bg
+            hl.TelescopeNormal = {
+                bg = c.bg,
+                fg = c.fg,
+            }
+            hl.TelescopeBorder = {
+                bg = c.bg,
+                fg = c.bg,
+            }
+            hl.TelescopePromptNormal = {
+                bg = prompt,
+            }
+            hl.TelescopePromptBorder = {
+                bg = prompt,
+                fg = prompt,
+            }
+            hl.TelescopePromptTitle = {
+                bg = c.bg_highlight,
+                fg = c.fg,
+            }
+            hl.TelescopePreviewTitle = {
+                bg = c.bg,
+                fg = c.bg,
+            }
+            hl.TelescopeResultsTitle = {
+                bg = c.bg,
+                fg = c.bg,
+            }
+            hl.NoiceCmdlinePopupBorder = {
+                bg = c.bg,
+                fg = c.bg,
+            }
+        end
+
+        require("tokyonight").setup {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            style = "night", -- The theme comes in four styles, `storm`, `moon`, a darker variant `night` and `day`
+            light_style = "day", -- The theme is used when the background is set to light
+            transparent = false, -- Enable this to disable setting the background color
+            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = true },
+                keywords = { italic = true },
+                functions = { italic = true },
+                variables = { italic = true },
+                -- Background styles. Can be "dark", "transparent" or "normal"
+                sidebars = "dark", -- style for sidebars, see below
+                floats = "dark", -- style for floating windows
+            },
+            sidebars = { "qf", "help", "terminal", "packer" }, -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+            day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+            hide_inactive_statusline = true, -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work with the standard **StatusLine** and **LuaLine**.
+            dim_inactive = false, -- dims inactive windows
+            lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
+
+            --- You can override specific color groups to use other groups or a hex color
+            --- function will be called with a ColorScheme table
+            ---@param colors ColorScheme
+            on_colors = function(colors)
+                -- colors.border = "#565f89"
+            end,
+
+            --- You can override specific highlights to use other groups or a hex color
+            --- function will be called with a Highlights and ColorScheme table
+            ---@param hl Highlights
+            ---@param c ColorScheme
+            on_highlights = function(hl, c)
+                highlight_treesitter_context(hl, c)
+                -- highlight_telescope(hl, c)
+
+                -- local prompt = "#2d3149"
+                -- hl.LineNr = {
+                --     fg = c.comment,
+                -- }
+                -- hl.TelescopeNormal = {
+                --     bg = c.bg_dark,
+                --     fg = c.fg_dark,
+                -- }
+                -- hl.TelescopeBorder = {
+                --     bg = c.bg_dark,
+                --     fg = c.bg_dark,
+                -- }
+                -- hl.TelescopePromptNormal = {
+                --     bg = prompt,
+                -- }
+                -- hl.TelescopePromptBorder = {
+                --     bg = prompt,
+                --     fg = prompt,
+                -- }
+                -- hl.TelescopePromptTitle = {
+                --     bg = prompt,
+                --     fg = prompt,
+                -- }
+                -- hl.TelescopePreviewTitle = {
+                --     bg = c.bg_dark,
+                --     fg = c.bg_dark,
+                -- }
+                -- hl.TelescopeResultsTitle = {
+                --     bg = c.bg_dark,
+                --     fg = c.bg_dark,
+                -- }
+                -- hl.FloatBorder = {
+                --     bg = c.none,
+                --     fg = c.comment,
+                -- }
+                -- hl.NormalFloat = {
+                --     bg = c.none,
+                -- }
+                -- hl.WhichKeyFloat = {
+                --     bg = c.none,
+                -- }
+            end,
+
+            cache = true, -- When set to true, the theme will be cached for better performance
+            plugins = {
+                -- enable all plugins when not using lazy.nvim
+                -- set to false to manually enable/disable plugins
+                all = package.loaded.lazy == nil,
+                -- uses your plugin manager to automatically enable needed plugins
+                -- currently only lazy.nvim is supported
+                auto = true,
+                -- add any plugins here that you want to enable
+                -- for all possible plugins, see:
+                --   * https://github.com/folke/tokyonight.nvim/tree/main/lua/tokyonight/groups
+                -- telescope = true,
+            },
+        }
+
+        vim.cmd [[colorscheme tokyonight]]
     end,
-
-    --- You can override specific highlights to use other groups or a hex color
-    --- function will be called with a Highlights and ColorScheme table
-    ---@param hl Highlights
-    ---@param c ColorScheme
-    on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.LineNr = {
-            fg = c.comment,
-        }
-        hl.TelescopeNormal = {
-            bg = c.bg_dark,
-            fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-            bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-            bg = prompt,
-            fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-            bg = prompt,
-            fg = prompt,
-        }
-        hl.TelescopePreviewTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-            bg = c.bg_dark,
-            fg = c.bg_dark,
-        }
-        hl.FloatBorder = {
-            bg = c.none,
-            fg = c.comment,
-        }
-        hl.NormalFloat = {
-            bg = c.none,
-        }
-        hl.WhichKeyFloat = {
-            bg = c.none,
-        }
-    end,
-})
-
-vim.cmd([[colorscheme tokyonight]])
+}

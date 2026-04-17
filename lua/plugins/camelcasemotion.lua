326b0dd2d84101b9d66beeeed3ee8649e0243fea
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:14:40 +0800

    feat(camelCaseMotion): Adapt to lazyvim

--- lua/plugins/camelcasemotion.lua
+++ lua/plugins/camelcasemotion.lua
@@ -1,1 +1,11 @@
-vim.g.camelcasemotion_key = ','
+return {
+    "bkad/camelcasemotion",
+    config = function()
+        vim.cmd([[
+            map <silent> ,w <Plug>CamelCaseMotion_w
+            map <silent> ,b <Plug>CamelCaseMotion_b
+            map <silent> ,e <Plug>CamelCaseMotion_e
+            map <silent> ,ge <Plug>CamelCaseMotion_ge
+        ]])
+    end,
+}

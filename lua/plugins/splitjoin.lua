4701bce4abc09735d759e9820ef8a491ae68a5e0
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:12:06 +0800

    feat(splitjoin): Adapt to lazyvim

--- lua/plugins/splitjoin.lua
+++ lua/plugins/splitjoin.lua
@@ -1,3 +1,8 @@
-vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
-vim.g.splitjoin_trailing_comma = 1
-vim.g.splitjoin_php_method_chain_full = 1
+return {
+    "AndrewRadev/splitjoin.vim",
+    config = function()
+        vim.g.splitjoin_html_attributes_bracket_on_new_line = 1
+        vim.g.splitjoin_trailing_comma = 1
+        vim.g.splitjoin_php_method_chain_full = 1
+    end,
+}

7ab61b77d39ab4b47e89c77eff463754a0a0e683
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:14:32 +0800

    feat(cleverf): Adapt to lazyvim

--- lua/plugins/cleverf.lua
+++ lua/plugins/cleverf.lua
@@ -1,2 +1,8 @@
-vim.g.clever_f_smart_case = 1
-vim.g.clever_f_mark_char_color = "Search"
+return {
+    "rhysd/clever-f.vim",
+    enable = false,
+    config = function()
+        vim.g.clever_f_smart_case = 1
+        vim.g.clever_f_mark_char_color = "Comment"
+    end,
+}

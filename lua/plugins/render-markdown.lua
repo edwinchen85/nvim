2bd3e708b938cd27f1615519dc0ec5c95d0b6503
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:18:53 +0800

    feat: add render-markdown.nvim

---
+++ lua/plugins/render-markdown.lua
@@ -0,0 +1,7 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+}

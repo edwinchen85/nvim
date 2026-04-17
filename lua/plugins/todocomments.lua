0830ebc00a0b48a2ff9b55e06e226a1fd5a41083
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:10:55 +0800

    feat(todocomments): Adapt to lazyvim

--- lua/plugins/todocomments.lua
+++ lua/plugins/todocomments.lua
@@ -1,1 +1,41 @@
-require("todo-comments").setup {}
+return {
+    "folke/todo-comments.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+        search = {
+            command = "rg",
+            args = {
+                "--color=never",
+                "--no-heading",
+                "--with-filename",
+                "--line-number",
+                "--column",
+                "--hidden",
+            },
+            pattern = [[\b(KEYWORDS):]],
+        },
+    },
+    keys = {
+        {
+            "<leader>ft",
+            function()
+                Snacks.picker.todo_comments({ hidden = true })
+            end,
+            desc = "Todo",
+        },
+        {
+            "]t",
+            function()
+                require("todo-comments").jump_next()
+            end,
+            desc = "Next todo comment",
+        },
+        {
+            "[t",
+            function()
+                require("todo-comments").jump_prev()
+            end,
+            desc = "Prev todo comment",
+        },
+    },
+}

74cde3626ab00f8b525b58216bd636cc707e2a02
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:14:55 +0800

    feat(alpha): Adapt to lazyvim

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,37 +1,44 @@
-local status_ok, alpha = pcall(require, "alpha")
-if not status_ok then
-    return
+local M = {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+}
+
+function M.config()
+    local dashboard = require("alpha.themes.dashboard")
+
+    dashboard.section.header.val = {
+        [[                                                                           ]],
+        [[                                                                         ]],
+        [[         ████ ██████           █████      ██                       ]],
+        [[        ███████████             █████                               ]],
+        [[        █████████ ███████████████████ ███   ███████████     ]],
+        [[       █████████  ███    █████████████ █████ ██████████████     ]],
+        [[      █████████ ██████████ █████████ █████ █████ ████ █████     ]],
+        [[    ███████████ ███    ███ █████████ █████ █████ ████ █████    ]],
+        [[   ██████  █████████████████████ ████ █████ █████ ████ ██████   ]],
+        [[                                                                           ]],
+    }
+
+    dashboard.section.buttons.val = {
+        dashboard.button("r", "  Recent files", "<cmd>lua Snacks.picker.recent()<CR>"),
+        dashboard.button(
+            "f",
+            "󰱼  Find file",
+            "<cmd>lua Snacks.picker.files({ finder = 'files', format = 'file', hidden = true, show_empty = true, supports_live = true })<CR>"
+        ),
+        dashboard.button("t", "  Find text", "<cmd>lua Snacks.picker.grep()<CR>"),
+        dashboard.button("c", "  Configuration", "<cmd>e ~/.config/nvim/init.lua<CR>"),
+        dashboard.button("q", "  Quit dashboard", "<cmd>Alpha<CR>"),
+    }
+
+    dashboard.section.footer.val = ""
+
+    dashboard.section.footer.opts.hl = "Type"
+    dashboard.section.header.opts.hl = "Include"
+    dashboard.section.buttons.opts.hl = "Keyword"
+
+    dashboard.opts.opts.noautocmd = true
+    require("alpha").setup(dashboard.opts)
 end
 
-local dashboard = require("alpha.themes.dashboard")
-
-dashboard.section.header.val = {
-    [[                                                                           ]],
-    [[                                                                         ]],
-    [[         ████ ██████           █████      ██                       ]],
-    [[        ███████████             █████                               ]],
-    [[        █████████ ███████████████████ ███   ███████████     ]],
-    [[       █████████  ███    █████████████ █████ ██████████████     ]],
-    [[      █████████ ██████████ █████████ █████ █████ ████ █████     ]],
-    [[    ███████████ ███    ███ █████████ █████ █████ ████ █████    ]],
-    [[   ██████  █████████████████████ ████ █████ █████ ████ ██████   ]],
-    [[                                                                           ]],
-}
-
-dashboard.section.buttons.val = {
-    dashboard.button("r", "  Recent files", ":Telescope oldfiles <CR>"),
-    dashboard.button("f", "󰱼  Find file", ":Telescope find_files <CR>"),
-    dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-    dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua <CR>"),
-    dashboard.button("q", "  Quit dashboard", ":Alpha <CR>"),
-}
-
-dashboard.section.footer.val = ""
-
-dashboard.section.footer.opts.hl = "Type"
-dashboard.section.header.opts.hl = "Include"
-dashboard.section.buttons.opts.hl = "Keyword"
-
-dashboard.opts.opts.noautocmd = true
-
-alpha.setup(dashboard.opts)
+return M

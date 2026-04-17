579789f21f2b262bca6d892b7890012ba81c98e6
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:14:47 +0800

    feat(autopairs): Adapt to lazyvim

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,52 +1,56 @@
-local npairs = require("nvim-autopairs")
-local Rule = require("nvim-autopairs.rule")
+return {
+    "windwp/nvim-autopairs",
+    config = function()
+        local npairs = require("nvim-autopairs")
+        local Rule = require("nvim-autopairs.rule")
+        local ts_conds = require("nvim-autopairs.ts-conds")
 
-require("nvim-autopairs").setup({
-    check_ts = true,
-    ts_config = {
-        lua = { "string", "source" },
-        javascript = { "string", "template_string" },
-        java = false,
-    },
-    disable_filetype = { "TelescopePrompt", "spectre_panel", "vue" },
-})
+        npairs.setup({
+            check_ts = true,
+            ts_config = {
+                lua = { "string", "source" },
+                javascript = { "string", "template_string" },
+                java = false,
+            },
+            disable_filetype = { "TelescopePrompt", "snacks_picker_input", "spectre_panel", "vue" },
+        })
 
-local ts_conds = require("nvim-autopairs.ts-conds")
+        -- press % => %% is only inside comment or string
+        npairs.add_rules({
+            Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
+            Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
+        })
 
--- press % => %% is only inside comment or string
-npairs.add_rules({
-    Rule("%", "%", "lua"):with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-    Rule("$", "$", "lua"):with_pair(ts_conds.is_not_ts_node({ "function" })),
-})
-
--- Add spaces between parentheses
-npairs.add_rules({
-    Rule(" ", " "):with_pair(function(opts)
-        local pair = opts.line:sub(opts.col - 1, opts.col)
-        return vim.tbl_contains({ "()", "[]", "{}" }, pair)
-    end),
-    Rule("( ", " )")
-        :with_pair(function()
-            return false
-        end)
-        :with_move(function(opts)
-            return opts.prev_char:match(".%)") ~= nil
-        end)
-        :use_key(")"),
-    Rule("{ ", " }")
-        :with_pair(function()
-            return false
-        end)
-        :with_move(function(opts)
-            return opts.prev_char:match(".%}") ~= nil
-        end)
-        :use_key("}"),
-    Rule("[ ", " ]")
-        :with_pair(function()
-            return false
-        end)
-        :with_move(function(opts)
-            return opts.prev_char:match(".%]") ~= nil
-        end)
-        :use_key("]"),
-})
+        -- Add spaces between parentheses
+        npairs.add_rules({
+            Rule(" ", " "):with_pair(function(opts)
+                local pair = opts.line:sub(opts.col - 1, opts.col)
+                return vim.tbl_contains({ "()", "[]", "{}" }, pair)
+            end),
+            Rule("( ", " )")
+                :with_pair(function()
+                    return false
+                end)
+                :with_move(function(opts)
+                    return opts.prev_char:match(".%)") ~= nil
+                end)
+                :use_key(")"),
+            Rule("{ ", " }")
+                :with_pair(function()
+                    return false
+                end)
+                :with_move(function(opts)
+                    return opts.prev_char:match(".%}") ~= nil
+                end)
+                :use_key("}"),
+            Rule("[ ", " ]")
+                :with_pair(function()
+                    return false
+                end)
+                :with_move(function(opts)
+                    return opts.prev_char:match(".%]") ~= nil
+                end)
+                :use_key("]"),
+        })
+    end,
+}

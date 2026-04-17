96766202f401df553a5f262fc40901d829bfbe75
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:20:39 +0800

    feat: add avante.lua

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,133 @@
+return {}
+-- return {
+--     "yetone/avante.nvim", -- A plugin for enhanced AI integration and functionality
+--     event = "VeryLazy", -- Triggered when the plugin is loaded lazily
+--     lazy = false,
+--     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+--     opts = {
+--         -- add any opts here
+--         -- for example
+--         provider = "claude",
+--         -- provider = "bitdeer",
+--         -- auto_suggestions_provider = nil,
+--         providers = {
+--             claude = {
+--                 endpoint = "https://llm.matrixport.io", -- Custom LLM endpoint for Claude provider
+--                 model = "claude-sonnet-4-6", -- AI model used for the Claude provider
+--                 timeout = 30000, -- Timeout in milliseconds
+--                 extra_request_body = {
+--                     temperature = 0.75, -- Controls randomness in responses (0=deterministic, 1=creative)
+--                     max_tokens = 20480, -- Maximum number of tokens in the response
+--                 },
+--             },
+--             openai = {
+--                 endpoint = "https://api.openai.com/v1",
+--                 model = "gpt-4o-mini", -- your desired model (or use gpt-4o, etc.)
+--                 timeout = 30000, -- timeout in milliseconds
+--                 -- temperature = 0, -- adjust if needed
+--                 -- max_tokens = 2048,
+--                 -- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+--             },
+--             bitdeer = {
+--                 __inherited_from = "openai",
+--                 api_key_name = "API_KEY",
+--                 endpoint = "https://staging-aider.bitdeer.ai/api/",
+--                 -- model = "DeepSeek-V3(vLLM FP8)",
+--                 -- model = "DeepSeek-V3-671B-FP8",
+--                 -- model = "DeepSeek-V3-671B-BF16",
+--                 -- model = "Llama-3.3-70B-Instruct",
+--                 model = "google/gemma-3-27b-it",
+--                 -- model = "Qwen/Qwen3-235B-A22B",
+--                 -- model = "deepseek-chat",
+--                 -- model = "deepseek-reasoner",
+--                 -- temperature = 0.5,
+--                 -- max_tokens = 4096,
+--             },
+--             -- gemini = {
+--             --     endpoint = "",
+--             --     model = "claude-3-5-sonnet-20241022",
+--             --     temperature = 0,
+--             --     max_tokens = 4096,
+--             -- },
+--         },
+--         mappings = {
+--             --- @class AvanteConflictMappings
+--             diff = {
+--                 ours = "co",
+--                 theirs = "ct",
+--                 all_theirs = "ca",
+--                 both = "cb",
+--                 cursor = "cc",
+--                 next = "]x",
+--                 prev = "[x",
+--             },
+--             suggestion = {
+--                 accept = "<M-l>",
+--                 next = "<M-]>",
+--                 prev = "<M-[>",
+--                 dismiss = "<C-]>",
+--             },
+--             jump = {
+--                 next = "]]",
+--                 prev = "[[",
+--             },
+--             submit = {
+--                 normal = "<CR>",
+--                 insert = "<C-s>",
+--             },
+--             sidebar = {
+--                 apply_all = "A",
+--                 apply_cursor = "a",
+--                 retry_user_request = "r",
+--                 edit_user_request = "e",
+--                 switch_windows = "<Tab>",
+--                 reverse_switch_windows = "<S-Tab>",
+--                 remove_file = "d",
+--                 add_file = "@",
+--                 close = { "<Esc>", "q" },
+--                 close_from_input = nil, -- e.g., { normal = "<Esc>", insert = "<C-d>" }
+--             },
+--         },
+--     },
+--     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+--     build = "make",
+--     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+--     dependencies = {
+--         "nvim-lua/plenary.nvim",
+--         "MunifTanjim/nui.nvim",
+--         --- The below dependencies are optional,
+--         "echasnovski/mini.pick", -- for file_selector provider mini.pick
+--         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+--         "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+--         "ibhagwan/fzf-lua", -- for file_selector provider fzf
+--         "stevearc/dressing.nvim", -- for input provider dressing
+--         "folke/snacks.nvim", -- for input provider snacks
+--         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+--         "zbirenbaum/copilot.lua", -- for providers='copilot'
+--         {
+--             -- support for image pasting
+--             "HakonHarnes/img-clip.nvim",
+--             event = "VeryLazy",
+--             opts = {
+--                 -- recommended settings
+--                 default = {
+--                     embed_image_as_base64 = false,
+--                     prompt_for_file_name = false,
+--                     drag_and_drop = {
+--                         insert_mode = true,
+--                     },
+--                     -- required for Windows users
+--                     use_absolute_path = true,
+--                 },
+--             },
+--         },
+--         {
+--             -- Make sure to set this up properly if you have lazy=true
+--             "MeanderingProgrammer/render-markdown.nvim",
+--             opts = {
+--                 file_types = { "markdown", "Avante" },
+--             },
+--             ft = { "markdown", "Avante" },
+--         },
+--     },
+-- }

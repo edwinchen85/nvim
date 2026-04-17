45404caa3bb5fa5f0fdd2603521ce9112e014629
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:15:36 +0800

    feat(luasnip): Adapt to lazyvim

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,5 +1,11 @@
--- Load custom snippets
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/typescript" } })
-require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/python" } })
+return {
+    "L3MON4D3/LuaSnip",
+    event = "InsertEnter",
+    config = function()
+        -- Load custom snippets
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/typescript" } })
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "./snippets/python" } })
 
-require("luasnip").filetype_extend("all", { "_" })
+        require("luasnip").filetype_extend("all", { "_" })
+    end,
+}

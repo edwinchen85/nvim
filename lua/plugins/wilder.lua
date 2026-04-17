35d48c00b5f689f0c33fb002767afec4b9c83d28
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:04:50 +0800

    feat(wilder): adapt to lazyvim

--- lua/plugins/wilder.lua
+++ lua/plugins/wilder.lua
@@ -1,28 +1,33 @@
-local wilder = require("wilder")
+return {
+    "gelguy/wilder.nvim",
+    config = function()
+        local wilder = require("wilder")
 
-wilder.setup({
-    modes = { ":" },
-    next_key = "<Tab>",
-    previous_key = "<S-Tab>",
-    accept_key = "<Space>",
-    reject_key = "<C-e>",
-    enable_cmdline_enter = 0,
-})
+        require("wilder").setup({
+            modes = { ":" },
+            next_key = "<Tab>",
+            previous_key = "<S-Tab>",
+            accept_key = "<Space>",
+            reject_key = "<C-e>",
+            enable_cmdline_enter = 0,
+        })
 
-wilder.set_option(
-    "renderer",
-    wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
-        highlighter = wilder.basic_highlighter(),
-        highlights = {
-            accent = wilder.make_hl(
-                "WilderAccent",
-                "Pmenu",
-                { { a = 1 }, { a = 1 }, { foreground = "#f4468f", background = "None" } }
-            ),
-            border = "FloatBorder", -- highlight to use for the border
-        },
-        -- 'single', 'double', 'rounded' or 'solid'
-        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
-        border = "rounded",
-    }))
-)
+        require("wilder").set_option(
+            "renderer",
+            wilder.popupmenu_renderer(wilder.popupmenu_border_theme({
+                highlighter = wilder.basic_highlighter(),
+                highlights = {
+                    accent = wilder.make_hl(
+                        "WilderAccent",
+                        "Pmenu",
+                        { { a = 1 }, { a = 1 }, { foreground = "#f4468f", background = "None" } }
+                    ),
+                    border = "FloatBorder", -- highlight to use for the border
+                },
+                -- 'single', 'double', 'rounded' or 'solid'
+                -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+                border = "rounded",
+            }))
+        )
+    end,
+}

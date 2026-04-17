b34cc04da9c6c8c3b1448cf585d83b74b1eba09c
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:13:59 +0800

    feat(harpoon): Adapt to lazyvim

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,10 +1,38 @@
-local h_status_ok, harpoon = pcall(require, "harpoon")
-if not h_status_ok then
-    return
+local M = {
+    "ThePrimeagen/harpoon",
+    event = "VeryLazy",
+    dependencies = {
+        { "nvim-lua/plenary.nvim" },
+    },
+}
+
+function M.config()
+    require("harpoon").setup({
+        -- menu = {
+        --     width = math.ceil(vim.api.nvim_win_get_width(0) / 7 * 3),
+        -- },
+        menu = {
+            width = 80,
+        },
+    })
+
+    local u = require("config.utils")
+    u.nmap("gm", "m") -- remap mark
+
+    local wk = require("which-key")
+    wk.add({
+        { "<leader>m", group = "Harpoon" },
+        { "<leader>ma", "<cmd>lua require('plugins.harpoon').mark_file()<cr>", desc = "Add Mark" },
+        { "<leader>mm", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Toggle Menu" },
+        { "<leader>mp", "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = "Prev Mark" },
+        { "<leader>mn", "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = "Next Mark" },
+    })
 end
 
-harpoon.setup({
-    menu = {
-        width = math.ceil(vim.api.nvim_win_get_width(0) / 3),
-    },
-})
+function M.mark_file()
+    require("harpoon.mark").add_file()
+    vim.notify("󱡅  marked file")
+end
+
+-- return M
+return {}

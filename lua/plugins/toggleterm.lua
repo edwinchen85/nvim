fd25d0cc979b7bdc978b4425d65c8b7a1a470cf7
Edwin Chen <edwinchen85@gmail.com>
2026-04-17 16:10:42 +0800

    feat(toggleterm): Adapt to lazyvim

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,17 @@
-require("toggleterm").setup({
-    size = 20,
-    open_mapping = [[<c-\>]],
-    hide_numbers = true, -- hide the number column in toggleterm buffers
-    shade_terminals = false,
-    start_in_insert = true,
-    insert_mappings = true, -- whether or not the open mapping applies in insert mode
-    persist_size = true,
-    direction = "horizontal",
-    close_on_exit = true, -- close the terminal window when the process exits
-    shell = vim.o.shell, -- change the default shell
-})
+return {
+    "akinsho/nvim-toggleterm.lua",
+    config = function()
+        require("toggleterm").setup({
+            size = 20,
+            open_mapping = [[<c-\>]],
+            hide_numbers = true, -- hide the number column in toggleterm buffers
+            shade_terminals = false,
+            start_in_insert = true,
+            insert_mappings = true, -- whether or not the open mapping applies in insert mode
+            persist_size = true,
+            direction = "horizontal",
+            close_on_exit = true, -- close the terminal window when the process exits
+            shell = vim.o.shell, -- change the default shell
+        })
+    end,
+}

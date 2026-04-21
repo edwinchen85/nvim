114f2cc576f97e0e8108366a4152c448d8aeac88
Edwin Chen <edwinchen85@gmail.com>
2026-04-21 22:05:10 +0800

    feat(cmp): Optimise event

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,6 @@
 return {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         { "hrsh7th/cmp-nvim-lsp", event = "InsertEnter" },
         { "hrsh7th/cmp-buffer", event = "InsertEnter" },

6a0632128705f22b50dc2334bdc554b8eff9bfe4
Edwin Chen <edwinchen85@gmail.com>
2026-04-21 21:12:10 +0800

    feat(mini): add splitjoin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -11,4 +11,20 @@ return {
             end
         end,
     },
+    -- Split & join
+    {
+        "echasnovski/mini.splitjoin",
+        config = function()
+            local miniSplitJoin = require("mini.splitjoin")
+            miniSplitJoin.setup({
+                mappings = { toggle = "" }, -- Disable default mapping
+            })
+            vim.keymap.set({ "n", "x" }, "gJ", function()
+                miniSplitJoin.join()
+            end, { desc = "Join arguments" })
+            vim.keymap.set({ "n", "x" }, "gS", function()
+                miniSplitJoin.split()
+            end, { desc = "Split arguments" })
+        end,
+    },
 }

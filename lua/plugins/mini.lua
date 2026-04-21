f570ca877590448c10d153a29a27de750e8197e5
Edwin Chen <edwinchen85@gmail.com>
2026-04-21 21:08:23 +0800

    refactor(mini): restructure mini

---
+++ lua/plugins/mini.lua
@@ -0,0 +1,14 @@
+return {
+    -- Icons
+    {
+        "echasnovski/mini.icons",
+        version = false,
+        opts = {},
+        init = function()
+            package.preload["nvim-web-devicons"] = function()
+                require("mini.icons").mock_nvim_web_devicons()
+                return package.loaded["nvim-web-devicons"]
+            end
+        end,
+    },
+}

be8e87649e00e21078500aeaf4a01c7e9f79ac10
Edwin Chen <edwinchen85@gmail.com>
2026-04-30 21:44:43 +0800

    chore(cleverf): remove plugin

--- lua/plugins/cleverf.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "rhysd/clever-f.vim",
-    enable = false,
-    config = function()
-        vim.g.clever_f_smart_case = 1
-        vim.g.clever_f_mark_char_color = "Comment"
-    end,
-}
